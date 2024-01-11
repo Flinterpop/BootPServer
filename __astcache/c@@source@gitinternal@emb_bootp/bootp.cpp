dd90e9f65044c690cdb46f794989d5bdf2bf1976
B Graham <139028622+Flinterpop@users.noreply.github.com>
2024-01-10 19:50:34 -0500

    Initial Commit
    Improved version from prototype tested in lab on 10 Jan 2024,

---
+++ __astcache/c@@source@gitinternal@emb_bootp/bootp.cpp
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#ifdef _WIN32
+#include <tchar.h>
+#endif
+#pragma hdrstop
+#include <System.StartUpCopy.hpp>
+//---------------------------------------------------------------------------
+USEFORM("Unit4.cpp", BootPProxy);
+//---------------------------------------------------------------------------
+extern "C" int FMXmain()
+{
+	try
+	{
+		Application->Initialize();
+		Application->CreateForm(__classid(TBootPProxy), &BootPProxy);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
+}
+//---------------------------------------------------------------------------
